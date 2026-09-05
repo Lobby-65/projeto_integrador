eced31762be77bce3bf7c22cb90cba1eb1a55920
Dcan1l0 <nuvensdanilo@gmail.com>
2026-09-05 00:25:10 -0300

    Add files via upload

--- INTEGRADOR/Projeto-Integrador/Backend/main.py
+++ INTEGRADOR/Projeto-Integrador/Backend/main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 from src.routes.login import login_bp
+from src.routes.cadastroConta import cadastroConta_bp
+from src.routes.index import index_bp
+
 
 # O "../" faz o caminho voltar um diretório acima do Backend
 app = Flask(__name__, template_folder="../front/templates",
@@ -9,7 +12,21 @@ app = Flask(__name__, template_folder="../front/templates",
 
 app.secret_key = "ChaveSecreta"
 
+
+#BLUEPRINTS DE CADA INTERFACE
+app.register_blueprint(cadastroConta_bp)
+app.register_blueprint(index_bp)
 app.register_blueprint(login_bp)
+
+
+#ROTAS ENTRE INTERFACES
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/cadastroConta')
+def cadastroConta():
+    return render_template('cadastroConta.html')
 
 
 if __name__ == '__main__':
